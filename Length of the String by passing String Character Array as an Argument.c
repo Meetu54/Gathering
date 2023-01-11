9257af1ee32e16cce863e19ed4de7e4e8a2df16d
Meet Savaliya <89479235+Meetu54@users.noreply.github.com>
2023-01-11 03:19:08 -0800

    Add files via upload

---
+++ Length of the String by passing String Character Array as an Argument.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+ 
+
+int Findlength(char str[]){
+   
+   int len = 0;
+   
+   
+   while (str[len] != '\0')
+   
+      len++;
+      
+   
+   
+   return (len);
+}
+ 
+main(){
+	
+   char str[1000];
+   
+   int length;
+   
+ 
+   printf("Enter the String :- ");
+   gets(str);
+ 
+   length = Findlength(str);
+ 
+   printf("Length of the String is :- %i", length);
+
+}
+ 
+
